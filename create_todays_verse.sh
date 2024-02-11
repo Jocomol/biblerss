9d46334d26d1f9f1332cf129d43002bd91dea673
Jocomol <jocomol@protonmail.com>
2024-02-11 20:01:20 +0100

    feat: new features

--- create_todays_verse.sh
+++ create_todays_verse.sh
@@ -2,12 +2,16 @@ today=$(date -u +%Y-%m-%d)
 grep -hr $today plans/* | while read -r p; do
     date=$(echo $p | cut -d "," -f 1 | sed 's/"//g')
     verses=$(echo $p | cut -d "," -f 2 | sed 's/"//g')
-    echo $verses
-    while read -r verse
-    do
-        echo $verse
-        versepath=$(echo $verse | sed  's/ /_/g')
-        hugo new $date-$versepath.md
-        echo $verse >> content/$date-$versepath.md
-    done < <(echo $verses | cut -f1 -d ";")
+    info=$(echo $p | cut -d "," -f 3 | sed 's/"//g')
+    OLDIFS=$IFS
+    IFS=";"
+    read -ra parts <<< "$verses"
+    IFS=$OLDIFS
+    for verse in "${parts[@]}"; do
+        versepath=$(echo $verse | sed  's/ //'| sed  's/ /_/g')
+        hugo new $versepath.md
+        echo \# $verse >> content/$versepath.md
+	echo $info  >> content/$versepath.md
+	echo "- SOLI DEO GLORIA" >> content/$versepath.md
+    done
 done
